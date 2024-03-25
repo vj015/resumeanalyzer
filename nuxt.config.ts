201e93c4e69dba07a3abff924b2679b4258549ee
Vipul Jain <vjain2376@gmail.com>
2024-03-25 21:53:36 +0530

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
   runtimeConfig: {
     openAi: {
       secretKey: process.env.NUXT_OPEN_AI_SECRET_KEY,
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>{{ "Hello from Resume Analyzer" }}</div>
   <div class="inp">
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
     <input type="file" name="resume" id="resume" />
   </div>
 </template>
