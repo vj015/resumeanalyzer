279a8fc80def728a9b66707ec8265cff4a52cbc7
Vipul Jain <vjain2376@gmail.com>
2024-03-27 23:13:42 +0530

    added basic image and template

--- app.vue
+++ app.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>
-    <NuxtPage />
+  <div class="relative">
+    <img
+      class="w-full h-screen"
+      src="assets/images/bg-main.jpg"
+      alt="Main Bg"
+    />
+    <div class="absolute top-0 right-0">
+      <button class="font-mono font-medium text-sky-500 dark:text-sky-400 border-sky-500 border-2 rounded mt-5 mr-8 p-2">CodeGPT</button>
+    </div>
+    <NuxtPage class="absolute top-0 left-0 right-0 bottom-0" />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>{{ "Hello from Resume Analyzer" }}</div>
-  <div class="inp">
-    <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    <input type="file" name="resume" id="resume" />
+  <div class="flex flex-col items-center mt-32 ml-5 mr-5">
+    <div
+      class="text-slate-900 font-extrabold text-4xl sm:text-5xl lg:text-6xl tracking-tight text-center dark:text-white"
+    >
+      {{
+        "Unleash Your Resume and Code Quality: Dive into the Cutting-Edge Capabilities of OpenAI with"
+      }}
+      <span class="font-mono font-medium text-sky-500 dark:text-sky-400">{{
+        "ResumeGPT"
+      }}</span>
+    </div>
+    <p
+      class="mt-6 text-lg text-slate-600 text-center max-w-3xl mx-auto dark:text-slate-400"
+    >
+      {{
+        "Revolutionize Your Resume and Coding Skills: Unleash the Power of OpenAI with ResumeGPT! Plus, Empower HR with Insightful Resume Parsing for Streamlined Recruitment"
+      }}
+    </p>
+
+    <div class="mt-10">
+      <input
+        type="file"
+        class="file-input file-input-bordered file-input-primary md:w-5/6 w-11/12 cursor-pointer font-mono font-medium text-sky-500 dark:text-sky-400"
+        accept="application/pdf"
+      />
+    </div>
   </div>
 </template>
