3f66f29613d1d3490365ed9efb1b5ee79de2932b
Vipul Jain <vjain2376@gmail.com>
2024-03-28 20:33:37 +0530

    Added success template

--- components/resumeInput.vue
+++ components/resumeInput.vue
@@ -1,12 +1,46 @@
 <template>
-  <input
-    type="file"
-    class="file-input file-input-bordered file-input-primary md:w-5/6 w-11/12 cursor-pointer font-mono font-medium text-sky-500 dark:text-sky-400"
-    accept="application/pdf"
-  />
-  <button
-    class="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 mt-5"
-  >
-    {{ "Analyze Resume " }}
-  </button>
+  <div>
+    <div>
+      <input
+        type="file"
+        class="file-input file-input-bordered file-input-primary md:w-5/6 w-11/12 cursor-pointer font-mono font-medium text-sky-500 dark:text-sky-400"
+        accept="application/pdf"
+      />
+      <button
+        class="bg-slate-900 hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 text-white font-semibold h-12 px-6 rounded-lg w-full flex items-center justify-center dark:bg-sky-500 dark:highlight-white/20 dark:hover:bg-sky-400 mt-5"
+      >
+        {{ "Analyze Resume " }}
+      </button>
+    </div>
+    <showResult
+      :name="name"
+      :nameText="nameText"
+      :positives="positives"
+      :positivesText="positivesText"
+      :negatives="negatives"
+      :negativesText="negativesText"
+      :improvements="improvements"
+      :improvementsText="improvementsText"
+    />
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      name: "Name",
+      nameText:
+        "Ipsum vero lorem et eos ipsum dolores erat sanctus voluptua. Et eos lorem sea stet tempor. Ea sed voluptua sanctus diam aliquyam at accusam, amet lorem invidunt clita magna at eirmod, vero duo diam sit ut labore ea amet gubergren. Accusam duo sed duo magna et justo erat. Invidunt diam rebum kasd labore et nonumy, at sit sadipscing et erat sit takimata. Rebum sit et magna magna lorem. No kasd clita eirmod erat ut sea. Sit rebum erat et no.",
+      positives: "Positives",
+      positivesText:
+        "Ipsum vero lorem et eos ipsum dolores erat sanctus voluptua. Et eos lorem sea stet tempor. Ea sed voluptua sanctus diam aliquyam at accusam, amet lorem invidunt clita magna at eirmod, vero duo diam sit ut labore ea amet gubergren. Accusam duo sed duo magna et justo erat. Invidunt diam rebum kasd labore et nonumy, at sit sadipscing et erat sit takimata. Rebum sit et magna magna lorem. No kasd clita eirmod erat ut sea. Sit rebum erat et no.",
+      negatives: "Negatives",
+      negativesText:
+        "Ipsum vero lorem et eos ipsum dolores erat sanctus voluptua. Et eos lorem sea stet tempor. Ea sed voluptua sanctus diam aliquyam at accusam, amet lorem invidunt clita magna at eirmod, vero duo diam sit ut labore ea amet gubergren. Accusam duo sed duo magna et justo erat. Invidunt diam rebum kasd labore et nonumy, at sit sadipscing et erat sit takimata. Rebum sit et magna magna lorem. No kasd clita eirmod erat ut sea. Sit rebum erat et no.",
+      improvements: "Improvements",
+      improvementsText:
+        "Ipsum vero lorem et eos ipsum dolores erat sanctus voluptua. Et eos lorem sea stet tempor. Ea sed voluptua sanctus diam aliquyam at accusam, amet lorem invidunt clita magna at eirmod, vero duo diam sit ut labore ea amet gubergren. Accusam duo sed duo magna et justo erat. Invidunt diam rebum kasd labore et nonumy, at sit sadipscing et erat sit takimata. Rebum sit et magna magna lorem. No kasd clita eirmod erat ut sea. Sit rebum erat et no.",
+    };
+  },
+};
+</script>
---
+++ components/showResult.vue
@@ -0,0 +1,93 @@
+<template>
+  <div>
+    <div class="flex flex-col justify-start items-start p-3 mb-5">
+      <h1 class="text-2xl md:text-4xl font-mono text-sky-500 dark:text-sky-400">
+        {{ name }}
+      </h1>
+      <p class="text-sm md:text-base text-slate-300">
+        {{ nameText }}
+      </p>
+    </div>
+    <div
+      class="h-full rounded-lg flex flex-col lg:flex-row justify-between items-center gap-3 mt-4 mb-4"
+    >
+      <div class="relative md:w-full">
+        <div
+          class="absolute -inset-2 rounded-lg bg-[conic-gradient(var(--tw-gradient-stops))] from-cyan-600 via-emerald-600 to-slate-600 opacity-50 blur-2xl"
+        ></div>
+        <div class="card w-full bg-base-100 shadow-xl md:w-100">
+          <div class="card-body">
+            <h2
+              class="text-xl md:text-2xl font-mono bg-gradient-to-r from-green-500 via-teal-500 to-green-500 text-transparent bg-clip-text sticky top-3 z-10 backdrop-blur-md rounded-lg p-1 -mt-4"
+            >
+              {{ positives }}
+            </h2>
+            <p
+              class="text-sm md:text-base text-slate-300 text-justify ml-2 mt-2"
+            >
+              {{ positivesText }}
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+      </div>
+      <div class="relative md:w-full">
+        <div
+          class="absolute -inset-2 rounded-lg bg-[conic-gradient(var(--tw-gradient-stops))] from-red-600 via-rose-600 to-red-600 opacity-50 blur-2xl"
+        ></div>
+        <div class="card w-full bg-base-100 shadow-xl md:w-100">
+          <div class="card-body">
+            <h2
+              class="text-xl md:text-2xl font-mono bg-gradient-to-r from-red-500 via-orange-500 to-red-500 text-transparent bg-clip-text sticky top-3 z-10 backdrop-blur-md rounded-lg p-1 -mt-4"
+            >
+              {{ negatives }}
+            </h2>
+            <p
+              class="text-sm md:text-base text-slate-300 text-justify ml-2 mt-2"
+            >
+              {{ negativesText }}
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+      </div>
+      <div class="relative md:w-full">
+        <div
+          class="absolute -inset-2 rounded-lg bg-[conic-gradient(var(--tw-gradient-stops))] from-blue-600 via-sky-600 to-orange-600 opacity-50 blur-2xl"
+        ></div>
+        <div class="card w-full bg-base-100 shadow-xl md:w-100">
+          <div class="card-body">
+            <h2
+              class="text-xl md:text-2xl font-mono bg-gradient-to-r from-teal-300 via-blue-200 to-teal-500 text-transparent bg-clip-text sticky top-3 z-10 backdrop-blur-md rounded-lg p-1 -mt-4"
+            >
+              {{ "Improvements" }}
+            </h2>
+            <p
+              class="text-sm md:text-base text-slate-300 text-justify ml-2 mt-2"
+            >
+              {{
+                "Dolore gubergren justo aliquyam stet rebum sea accusam aliquyam nonumy et. Ipsum est sanctus et rebum. Lorem clita elitr dolor et diam. Sanctus sadipscing dolore consetetur dolor eos et voluptua sit, sea aliquyam accusam amet est nonumy vero sea, dolor et sed justo ea sadipscing sed ipsum aliquyam amet, clita amet rebum nonumy rebum dolor, eirmod ut sadipscing et dolores sed accusam dolor, sadipscing tempor et et dolor takimata sit,."
+              }}
+            </p>
+            <div class="card-actions justify-end"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {},
+  props: [
+    "name",
+    "nameText",
+    "positives",
+    "positivesText",
+    "negatives",
+    "negativesText",
+    "improvements",
+    "improvementsText",
+  ],
+};
+</script>
