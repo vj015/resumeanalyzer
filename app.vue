5c31fff7762a6b16a6201726bd493967c0a98b9c
Vipul Jain <vjain2376@gmail.com>
2024-03-29 01:27:28 +0530

    fixed nav

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>
-    <div class="fixed top-0 sm:right-0 right-12">
-      <button
-        class="font-mono font-medium text-sky-500 dark:text-sky-400 border-sky-500 border-2 rounded mt-5 mr-8 p-2"
-        @click="enablecodeGPT()"
-      >
-        {{ resumeGPT ? `CodeGPT` : `ResumeGPT` }}
-      </button>
-    </div>
+  <div class="flex flex-col items-center md:items-end">
+    <button
+      class="font-mono font-medium text-sky-500 dark:text-sky-400 border-sky-500 border-2 rounded mt-5 mr-1 p-2 w-4/12 md:w-2/12"
+      @click="enablecodeGPT()"
+    >
+      {{ resumeGPT ? `CodeGPT` : `ResumeGPT` }}
+    </button>
     <NuxtPage :resumeGPT="resumeGPT" />
   </div>
 </template>
