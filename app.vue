13ed0fb00564bbc0c0cb2bff9ace6d95cfa6cb02
Vipul Jain <vjain2376@gmail.com>
2024-07-10 20:57:00 +0530

    added error infor

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="relative flex flex-col items-center md:items-end">
+  <div
+    class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4"
+  >
+    <p class="font-bold underline">Error!</p>
+    <p>
+      API Quota of Openai keys have expired! Please reach
+      <span class="font-extrabold"><a href="https://vipuljain.in/">Vipul</a></span> to view the recorded demo of
+      this application!
+    </p>
+  </div>
+  <div class="relative flex flex-col items-center md:items-end mb-3">
     <button
       class="font-mono font-medium text-sky-500 dark:text-sky-400 border-sky-500 border-2 rounded mt-5 mr-1 p-2 w-4/12 md:w-2/12"
       @click="enablecodeGPT()"
